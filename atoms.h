de19939499769334840e12b5c92e09be5deca212
Naris R <nariscatboy@gmail.com>
2017-05-06 03:31:11 +1000

    Update atoms.h

--- atoms.h
+++ atoms.h
@@ -21,58 +21,58 @@ typedef struct gamestate_t gamestate_t;
 typedef union move_data_t move_data_t;
 
 union move_data_t {
-    struct {
-        uint8_t x;
-        uint8_t y;
-        uint16_t padding;
-    } component; 
-    
-    uint32_t raw_move;
+	struct {
+		uint8_t x;
+		uint8_t y;
+		uint16_t padding;
+	} component; 
+
+	uint32_t raw_move;
 };
 
 struct move_t {
-  move_data_t pos;
-  move_t* parent;
-  move_t* extra;
-  player_t* old_owner; // NULL if unoccupied
+	move_data_t pos;
+	move_t* parent;
+	move_t* extra;
+	player_t* old_owner; // NULL if unoccupied
 };
 
 struct game_t {
-  move_t* last;
+	move_t* last;
 };
 
 struct grid_t {
-  player_t* owner;
-  int atom_count;
+	player_t* owner;
+	int atom_count;
 };
 
 struct player_t {
-  const char* colour;
-  int grids_owned;
+	const char* colour;
+	int grids_owned;
 };
 
 struct gamestate_t {
-    uint8_t width;
-    uint8_t height;
-    
-    player_t* player;
-    uint8_t no_players;
-    
-    grid_t** board;
-    
-    uint32_t* raw_move_data;
-    size_t max_turns;
-    size_t turn;
-    uint8_t whose_turn;
-    game_t* moves;
-    
-    char* msg;
-    
-    int game_over;
+	uint8_t width;
+	uint8_t height;
+
+	player_t* player;
+	uint8_t no_players;
+
+	grid_t** board;
+
+	uint32_t* raw_move_data;
+	size_t max_turns;
+	size_t turn;
+	uint8_t whose_turn;
+	game_t* moves;
+
+	char* msg;
+
+	int game_over;
 };
 
 static const char * const colour[] = {
-    "Red", "Green", "Purple", "Blue", "Yellow", "White"
+	"Red", "Green", "Purple", "Blue", "Yellow", "White"
 };
 
 void print_turn(player_t* player, uint8_t whose_turn);
