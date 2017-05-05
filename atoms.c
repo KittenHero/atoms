2e4f83e45d5dbd5713ff0160364f6bac5ddce127
Naris R <nariscatboy@gmail.com>
2017-05-06 00:43:33 +1000

    Update atoms.c

--- atoms.c
+++ atoms.c
@@ -91,7 +91,7 @@ int check_winner(gamestate_t* data) {
 // function-like macros are for scrubs
 static inline int limit(int x, int y, int w, int h) { return 2 + (x && x + 1 < w) + (y && y + 1 < h); }
 
-static move_t* place_q(uint8_t x, uint8_t y, move_t* parent, gamestate_t* data) {
+move_t* place_q(uint8_t x, uint8_t y, move_t* parent, gamestate_t* data) {
 	if (data->game_over) return NULL;
 	int lim = limit(x, y, data->width, data->height);
 	move_t* cur = calloc(1, sizeof(move_t));
